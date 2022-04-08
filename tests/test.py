0bc95502bb02c9c1a4f72707d128c1827c054d83
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-04-08 16:26:19 +0200

    created template and minimum test

---
+++ tests/test.py
@@ -0,0 +1,44 @@
+import pytest
+import os
+from starkware.starknet.testing.starknet import Starknet
+import asyncio
+
+@pytest.fixture(scope="session")
+def name (pytestconfig):
+    return pytestconfig.getoption("name")
+
+def print_name (pytestconfig):
+    print(f"name: {pytestconfig.getoption('name')}")
+
+@pytest.mark.asyncio
+async def test (name):
+
+    starknet = await Starknet.empty()
+    print()
+
+    path = f'contracts/{name}/vlq.cairo'
+    contract = await starknet.deploy(path)
+    print(f"> Testing: {path}")
+
+    if name == 'template':
+        with pytest.raises(Exception) as e_info:
+            ret = await contract.convert_numerical_felt_to_vlq_literal(0).call()
+        with pytest.raises(Exception) as e_info:
+            ret = await contract.convert_vlq_literal_to_numerical_felt(0).call()
+        return
+
+    #
+    # Test cases
+    # TODO: add more cases
+    #
+    nums = [0, 128]
+    vlqs = [12336, 942747696]
+
+    for (num, vlq) in zip (nums, vlqs):
+        ret = await contract.convert_numerical_felt_to_vlq_literal(num).call()
+        assert ret.result.vlq == vlq
+
+        ret = await contract.convert_vlq_literal_to_numerical_felt(vlq).call()
+        assert ret.result.num == num
+
+    print(f" {name} has passed the test.")
